d4025f522649a42417d9ea47cec99f6956c14649
Riddhesh1009 <riddheshmalirm@gmail.com>
2023-07-13 18:17:04 +0530

    Added sections in home page

---
+++ src/components/Features.jsx
@@ -0,0 +1,25 @@
+import f1 from "../assets/F1.jpg";
+import f2 from "../assets/F2.jpg";
+import f3 from "../assets/F3.jpg";
+
+const FeatureBox = ({ src, name, desc }) => {
+    return (
+        <div className="w-full">
+            <img src={src} alt="features" className="w-full h-auto" />
+            <p className="font-semibold text-lg mt-5">{name}</p>
+            <p className="font-normal text-sm text-gray-800 w-[80%] mt-2">{desc}</p>
+        </div>
+    )
+}
+
+const Features = () => {
+    return (
+        <div className="grid grid-cols-1 md:grid-cols-3 w-full mx-auto md:container md:w-[63%] mb-20 gap-5">
+            <FeatureBox src={f1} name="FEATURE 1" desc="Talk about some of the details of your offer with a focus on the value people get back."/>
+            <FeatureBox src={f2} name="FEATURE 2" desc="Talk about some of the details of your offer with a focus on the value people get back."/>
+            <FeatureBox src={f3} name="FEATURE 3" desc="Talk about some of the details of your offer with a focus on the value people get back."/>
+        </div>
+    )
+}
+
+export default Features
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,18 @@
+import AboutUs from "./AboutUs"
 import Benefits from "./Benifits"
+import Features from "./Features"
 import LandingPage from "./LandingPage"
+import Reviews from "./Reviews"
+
 const Layout = () => {
     return (
-        <div>
+        <div className="">
             <LandingPage />
             {/* below sections */}
             <Benefits />
+            <Reviews/>
+            <Features/>
+            <AboutUs/>
         </div>
     )
 }
