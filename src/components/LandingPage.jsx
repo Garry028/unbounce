c08e67972d032a8b9bb8bf61c5cddadbd274a5cd
Garry <gmisal2002@gmail.com>
2023-07-13 12:28:27 +0530

    push

---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,27 @@
+import img from "../assets/img2.jpg";
+import logo from "../assets/logo.svg";
+
+const LandingPage = () => {
+    return (
+        <div
+            className="h-screen bg-cover bg-center flex items-start"
+            style={{ backgroundImage: `url(${img})` }}
+        >
+            <div className="container mx-auto text-white px-8">
+                <div className="flex items-center justify-start"> {/* Center the logo on small screens and align to start on medium screens and above */}
+                    <img src={logo} alt="Logo" className="h-44 w-44 ml-2" />
+                    {/* Replace 'h-32' and 'w-32' with your actual logo size */}
+                </div>
+                <h1 className="text-4xl md:text-6xl font-bold mt-8 mb-4 max-w-4xl text-teal-950"> {/* Decrease font size on small screens */}
+                    Describe the <br />value of booking an appointment
+                </h1>
+                <p className="text-lg md:text-2xl mb-8 max-w-3xl text-teal-800"> {/* Decrease font size on small screens */}
+                    No need to get clever. Tell people exactly what <br /> youre offering, then use this space to <br/>  communicate your key value proposition.
+                </p>
+                {/* Add more content or components as needed */}
+            </div>
+        </div>
+    );
+};
+
+export default LandingPage;
---
+++ src/components/Layout.jsx
@@ -0,0 +1,11 @@
+import LandingPage from "./LandingPage"
+const Layout = () => {
+    return (
+        <div>
+            <LandingPage />
+            {/* below sections */}
+        </div>
+    )
+}
+
+export default Layout
